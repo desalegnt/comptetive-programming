428f8e607a1715990b70f44caf732be7457e7073
desalegnt <126424280+desalegnt@users.noreply.github.com>
2023-03-06 03:23:03 +0300

    Create selectionsort.py

---
+++ selectionsort.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+class Solution: 
+    def select(self, arr, i):
+        # code here 
+        for j in range(i,-1,-1):
+            if arr[j] > arr[j + 1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+        return arr
+
+        
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(n-1):
+            arr = self.select(arr,i)
+        return arr
